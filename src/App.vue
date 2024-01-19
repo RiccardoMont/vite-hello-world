95165b18488bba9a3855b770f80a0e1410f14d39
RiccardoMont <riccardomontalto94@gmail.com>
2024-01-19 10:51:33 +0100

    h1 inserito insieme a dello stile

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <header>
+      <h1>Il mio primo h1 in un file Vue</h1>
+    </header>
+    
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+h1{
+  text-align: center;
+  font-weight: bold;
+  background: -webkit-linear-gradient(#0bec0b, #c500ff);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 </style>
